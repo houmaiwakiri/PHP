a95358f33883b9cc142d871ff3bea643d2e506a3
houmaiwakiri <mahomahoma888@gmail.com>
2021-11-03 02:11:29 +0900

    test

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-echo "Hello World!";
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css"> 
+    <meta charset='utf-8'>
+    <title>PHP練習</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo 'Hello world!';
+        ?>
+    </h1>
+</body>
+</html>
