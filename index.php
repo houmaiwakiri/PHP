9d27579901184a0627dcda6dab80c83da5c1b03c
houmaiwakiri <mahomahoma888@gmail.com>
2021-11-05 14:46:35 +0900

    難しいので一旦逃げる

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css"> 
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta charset='utf-8'>
     <title>PHP練習</title>
 </head>
 <body>
-    <h1>
-        <?php 
-        echo 'Hello world!';
-        ?>
-    </h1>
+    <div class="test">ヘッダー</div>
+    <?php 
+        include("C:\Users\user\OneDrive\ドキュメント\制作物\PHP\PHP\test.php"); 
+    ?>  
 </body>
 </html>
---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section id="news">
+        <h1>ニュース</h1>
+        <p>１ 新着ニュース　</p>
+        <p>２ 新着ニュース　</p>
+    </section>
+    
+</body>
+</html>
